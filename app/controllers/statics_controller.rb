7771e18ee6f5d7ffd5d5d3f2015aa047f00d95fb
chansecampbell <chansecampbell@gmail.com>
2016-04-23 13:18:53 +0100

    Commiting before I create a third model and potentially break everything.

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,4 +1,9 @@
 class StaticsController < ApplicationController
+  before_filter :authenticate_user!, except: [:about] 
+  
   def home
+  end
+
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'statics#home'
+  get '/about', to: "statics#about"
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
      resources :users, except: [:new, :create]
      resources :skills
