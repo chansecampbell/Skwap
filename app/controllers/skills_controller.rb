abe02cbb7aac0cdcec3973369986910cd7686c7f
chansecampbell <chansecampbell@gmail.com>
2016-04-23 12:43:45 +0100

    Got the skills index page to link to individual show pages.

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -8,9 +8,17 @@ class SkillsController < ApplicationController
   end
 
   def show
+    @skill = Skill.find(params[:id])
   end
 
   def create
+      @skill = current_user.skills.new(skill_params)
+      if @skill.save
+          flash[:success] = "Your service has been listed!"
+          redirect_to skills_path
+      else
+          render "new"
+      end
   end
 
   def edit
@@ -21,4 +29,13 @@ class SkillsController < ApplicationController
 
   def destroy
   end
+
+
+
+
+  private
+      def skill_params
+          params.require(:skill).permit(:title, :description, :response_time)
+      end
+
 end
