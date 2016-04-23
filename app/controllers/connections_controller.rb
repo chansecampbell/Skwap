774bff7576862fc93ff42b9e5a03ab66ea487c0b
chansecampbell <chansecampbell@gmail.com>
2016-04-23 14:15:20 +0100

    My third model connection now has a member route built ontop of the skill id. It takes me to a new connection form where I can make a request

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,6 +1,29 @@
 class ConnectionsController < ApplicationController
 
-  def new
-  end
-  
+ def new
+     @skill = Skill.find(params[:skill_id])
+     @connection = Connection.new
+ end
+
+ def create
+     @skill = Skill.find(params[:skill_id])
+     @connection = @skill.connections.new(connection_params)
+     @connection.sender_id = current_user.id
+     @connection.receiver_id = @skill.user.id
+     
+     if @connection.save
+         flash[:success] = "Your enquiry was made successfully"
+         redirect_to skills_path
+     else
+         flash[:danger] = "Your enquiry was not sent"
+         redirect_to skills_path
+     end
+ end
+
+
+ private
+ def connection_params
+     params.require(:connection).permit(:details, :meetup_time, :skill_id)
+ end
+
 end
