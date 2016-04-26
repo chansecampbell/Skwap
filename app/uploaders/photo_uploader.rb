df31f51eeedece6d921e44d6fce87cd3493ef4fd
chansecampbell <chansecampbell@gmail.com>
2016-04-26 15:36:57 +0100

    RMagick now working! Commiting before I sort out seeded images

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 class PhotoUploader < CarrierWave::Uploader::Base
+  include CarrierWave::RMagick
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
@@ -32,9 +33,13 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+    version :thumb do
+      process :resize_to_fill => [100, 100]
+    end
+
+    version :detail do
+      process :resize_to_fit => [400, 400]
+    end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
