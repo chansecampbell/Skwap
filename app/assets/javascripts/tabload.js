6a6fd48bb96730e0218f0b473fb8dc33e5273a91
chansecampbell <chansecampbell@gmail.com>
2016-04-27 14:52:52 +0100

    FINALLY got the learn profile page to display properly and list all cards

--- app/assets/javascripts/tabload.js
+++ app/assets/javascripts/tabload.js
@@ -1,11 +1,23 @@
 // Error in Foundation jQuery code means tab not auto selected on page load, so had to write this
 
 $(document).ready(function() {
-  var tabHash = location.hash;
-  
-  $(tabHash).toggleClass("active");
-  var anchorTagToSelect = $("a[href='" + tabHash + "']")
-  anchorTagToSelect.parent().toggleClass("active");
+
+
+  function fixTabs(callback) {
+    var tabHash = location.hash;
+    $(tabHash).toggleClass("active");
+    var anchorTagToSelect = $("a[href='" + tabHash + "']")
+    anchorTagToSelect.parent().toggleClass("active");
+    // callback();
+  }
+   
+  function scrollToTop() {
+    $('body').scrollTo('0px');
+  }
+
+  (function() {
+    fixTabs(scrollToTop);
+  })();
 
   $(".tab-title").click(function() {
     var tab = $(this);
